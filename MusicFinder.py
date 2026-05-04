8f59055fd8865eb30ed069c3cc2d4c0369095a3c
dmytro-s72 <dimon.soldatov23.08@gmail.com>
2026-05-04 22:50:28 +0300

    Updater MusicFinder.py

--- MusicFinder.py
+++ MusicFinder.py
@@ -12,7 +12,7 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, FSInputFil
 logging.basicConfig(level=logging.INFO)
 
 # 2. Змінні (токен вже вписано)
-TOKEN = "..."
+TOKEN = os.getenv("BOT_TOKEN")
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
